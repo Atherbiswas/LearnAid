845b3ccbc995d3d32b21884a0e08b284f38b02e9
Ather1230 <ather.biswas0057@gmail.com>
2022-10-11 00:29:57 +0600

    added navbar

--- src/App.js
+++ src/App.js
@@ -1,10 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
 import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+import Main from './layouts/Main';
+import Home from './components/Home/Home';
+import Courses from './components/Courses/Courses';
+import Statistics from './components/Statistics/Statistics';
+import Blog from './components/Blog/Blog';
 
 function App() {
   const router = createBrowserRouter([
-
+    {
+      path: '/',
+      element: <Main></Main>,
+      children: [
+        {
+          path: '/',
+          element: <Home></Home>
+        },
+        {
+          path: '/courses',
+          element: <Courses></Courses>
+        },
+        {
+          path: '/statistics',
+          element: <Statistics></Statistics>
+        },
+        {
+          path: '/blog',
+          element: <Blog></Blog>
+        }
+      ]
+    },
+    {
+      path: '*',
+      element: <h4 className='text-center mt-5'> <span className='fs-1 fw-bold'>404</span><br />Oops! This route is not avilable</h4>
+    }
   ])
   return (
     <div>
---
+++ src/components/Header/Header.js
@@ -0,0 +1,29 @@
+import logo from '../../favicon.png'
+import Container from 'react-bootstrap/Container';
+import Nav from 'react-bootstrap/Nav';
+import Navbar from 'react-bootstrap/Navbar';
+import { Link } from 'react-router-dom';
+import './Header.css'
+        
+function Header() {
+    return (
+            <Navbar bg="info" expand="lg">
+              <Container>
+              {/* <img src={logo} className='nav-img' alt="" /> */}
+                <Navbar.Brand to="/" className='fs-4 fw-bold'> <img src={logo} className='nav-img' alt="" /> <span className='fs-2 title-word'>L</span>-earniFy</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="basic-navbar-nav">
+                  <Nav className="ms-auto fs-5 link-style">
+                    <Link to='/'>Home</Link>
+                    <Link to='/courses'>Courses</Link>
+                    <Link to="/statistics">Statistics</Link>
+                    <Link to="/blog">Blog</Link>
+                    <Link to="/about">About</Link>
+                  </Nav>
+                </Navbar.Collapse>
+              </Container>
+            </Navbar>
+          );
+        }
+        
+ export default Header;
