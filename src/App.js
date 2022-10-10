bf23780495f9a03d63221ab613cdc6ee9386ee34
Ather1230 <ather.biswas0057@gmail.com>
2022-10-10 22:36:29 +0600

    router setup

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
 function App() {
+  const router = createBrowserRouter([
+
+  ])
   return (
     <div>
-      
+      <RouterProvider router={router}></RouterProvider>
     </div>
   );
 }
