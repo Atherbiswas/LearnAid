2af6e6f94156fe990844d58a15ae9b70ed33b263
Ather1230 <ather.biswas0057@gmail.com>
2022-10-11 23:00:47 +0600

    added toastify

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,23 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
 import './Quiz.css'
 import { ToastContainer, toast } from 'react-toastify';
-  import 'react-toastify/dist/ReactToastify.css';
+import 'react-toastify/dist/ReactToastify.css';
+
 
 const Quiz = ({quiz}) => {
-    // const [option, setOption] = useState('');
-    const [rightAnswer, setRightAnswer] = useState('');
-    console.log(rightAnswer)
     const {question, options, correctAnswer} = quiz;
     const handleClick = (option) =>{
-        console.log(option)
         if(correctAnswer === option){
-            setRightAnswer('Right Answer');
-            notify('Right Answer');
+            notify('YAY! Right Answer');
         }
         else if(correctAnswer !== option){
-            setRightAnswer('false');
-            notify('false');
-            // console.log('false');
+            notify('Oops! Wrong Answer');
         }
     }
     const notify = (message) => toast(message);
@@ -27,14 +21,13 @@ const Quiz = ({quiz}) => {
             <h2>{question}</h2>
             <div>
             {
-                options.map(option => <div className='border border-1 mb-1 w-75 container rounded-4 text-start p-2 bg-cadetblue fs-5'
+                options.map(option => <div className='bg-cadetblue border border-1 mb-1 w-75 container rounded-4 text-start p-2 fs-5'
                 key = {option}
                 > 
                     <input onClick={() => handleClick(option)} type="radio" id="html" name="fav_language" value="HTML"/><label>{option}</label>
                 </div>)
             }
             </div>
-            {/* <button onClick={notify}>Notify!</button> */}
         <ToastContainer />
         </div>
     );
