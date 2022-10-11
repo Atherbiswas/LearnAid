724dab9716499d1372a6c4b31ed2facbe37b9c83
Ather1230 <ather.biswas0057@gmail.com>
2022-10-11 22:17:47 +0600

    added quiz answer

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -2,22 +2,27 @@ import logo from '../../favicon.png'
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 import './Header.css'
         
 function Header() {
+  let activeClass = {
+    color: '#fff'
+  }
     return (
-            <Navbar bg="info" expand="lg">
+            <Navbar bg="info" expand="lg" className=''>
               <Container>
               {/* <img src={logo} className='nav-img' alt="" /> */}
                 <Navbar.Brand to="/" className='fs-4 fw-bold'> <img src={logo} className='nav-img' alt="" /> <span className='fs-2 title-word'>L</span>-earnAid</Navbar.Brand>
                 <Navbar.Toggle aria-controls="basic-navbar-nav" />
                 <Navbar.Collapse id="basic-navbar-nav">
                   <Nav className="ms-auto fs-5 link-style">
-                    <Link to='/'>Home</Link>
-                    <Link to="/statistics">Statistics</Link>
-                    <Link to="/blog">Blog</Link>
-                    <Link to="/about">About</Link>
+                    <NavLink  to='/' className={({isActive}) =>
+                  isActive? activeClass : undefined}>Home</NavLink>
+                    <NavLink to="/statistics" className={({isActive}) =>
+                  isActive? activeClass : undefined}>Statistics</NavLink>
+                    <NavLink to="/blog">Blog</NavLink>
+                    <NavLink to="/about">About</NavLink>
                   </Nav>
                 </Navbar.Collapse>
               </Container>
--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -1,18 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './Quiz.css'
+import { ToastContainer, toast } from 'react-toastify';
+  import 'react-toastify/dist/ReactToastify.css';
 
 const Quiz = ({quiz}) => {
-    const {question, options} = quiz;
+    // const [option, setOption] = useState('');
+    const [rightAnswer, setRightAnswer] = useState('');
+    console.log(rightAnswer)
+    const {question, options, correctAnswer} = quiz;
+    const handleClick = (option) =>{
+        console.log(option)
+        if(correctAnswer === option){
+            setRightAnswer('Right Answer');
+            notify('Right Answer');
+        }
+        else if(correctAnswer !== option){
+            setRightAnswer('false');
+            notify('false');
+            // console.log('false');
+        }
+    }
+    const notify = (message) => toast(message);
+    
     return (
-        <div className='container text-center border border-1 mb-2 shadow-lg'>
+        <div className='container text-center border border-1 rounded-4 mb-2 shadow-lg'>
             <h2>{question}</h2>
             <div>
             {
-                options.map(option => <div className='border border-1 mb-1 w-75 container text-start p-2 bg-cadetblue fs-5'> 
-                    <input type="radio" id="html" name="fav_language" value="HTML"/><label for="html">{option}</label>
+                options.map(option => <div className='border border-1 mb-1 w-75 container rounded-4 text-start p-2 bg-cadetblue fs-5'
+                key = {option}
+                > 
+                    <input onClick={() => handleClick(option)} type="radio" id="html" name="fav_language" value="HTML"/><label>{option}</label>
                 </div>)
             }
             </div>
+            {/* <button onClick={notify}>Notify!</button> */}
+        <ToastContainer />
         </div>
     );
 };
