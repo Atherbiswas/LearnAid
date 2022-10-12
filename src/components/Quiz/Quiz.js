d3092e1e3ea6b32cd3e0ce1b9fcd43d0e8c9dee9
Ather1230 <ather.biswas0057@gmail.com>
2022-10-12 17:19:26 +0600

    correct answer show by toast

--- src/components/Quiz/Quiz.js
+++ src/components/Quiz/Quiz.js
@@ -17,17 +17,20 @@ const Quiz = ({quiz}) => {
         }
     }
     const notify = (message) => toast(message);
+    const handleIcon =() => {
+        notify(correctAnswer);
+    }
     
     return (
         <div className='container text-center border border-1 rounded-4 mb-2 shadow-lg'>
-            <h2>{question}</h2>
+            <h2>Q:{question}</h2>
             <div>
-                <p className='text-end'><FontAwesomeIcon icon={faEye}></FontAwesomeIcon></p>
+                <p className='text-end'><FontAwesomeIcon onClick={handleIcon} icon={faEye}></FontAwesomeIcon></p>
             {
                 options.map(option => <div className='bg-cadetblue border border-1 mb-1 w-75 container rounded-4 text-start p-2 fs-5'
                 key = {option}
                 > 
-                    <input onClick={() => handleClick(option)} type="radio" id="html" name="fav_language" value="HTML"/><label>{option}</label>
+                    <input onClick={() => handleClick(option)} type="radio" id="option" value="option"/><label>{option}</label>
                 </div>)
             }
             </div>
